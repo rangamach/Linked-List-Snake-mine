6d22d89a82dd22e09ca07c153685dc2d6e1cffbd
rangamach <117797904+rangamach@users.noreply.github.com>
2025-01-17 13:47:32 +0530

    Initialization body part

--- Linked-List-Snake/include/Player/BodyPart.h
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -10,10 +10,23 @@ namespace Player
 	{
 	protected:
 		UI::UIElement::ImageView* bodypart_image;
-		sf::Vector2i grid_position;
-		Direction direction;
+		sf::Vector2i bodypart_grid_position;
+		Direction bodypart_direction;
 		float bodypart_width;
 		float bodypart_height;
+
+	private:
+		void CreateBodyPartImage();
+		void InitializeBodyPartImage();
+		sf::Vector2f GetBodyPartScreenPosition();
+		void Destroy();
+
+	public:
+		BodyPart();
+		~BodyPart();
+
+		void Initialize(float width, float height, sf::Vector2i position, Direction direction);
+		void Render();
 	};
 }
 
--- Linked-List-Snake/source/Player/BodyPart.cpp
+++ Linked-List-Snake/source/Player/BodyPart.cpp
@@ -1,3 +1,58 @@
 #include "../../include/Player/BodyPart.h"
+#include "../../include/UI/UIElement/ImageView.h"
+#include "../../include/Global/Config.h"
+#include "../../include/Level/LevelView.h"
 
+using namespace Global;
 using namespace Player;
+using namespace Level;
+
+void BodyPart::CreateBodyPartImage()
+{
+	bodypart_image = new UI::UIElement::ImageView();
+}
+
+void BodyPart::InitializeBodyPartImage()
+{
+	bodypart_image->initialize(Config::snake_body_texture_path, bodypart_width, bodypart_height, GetBodyPartScreenPosition());
+	bodypart_image->setOriginAtCentre();
+}
+
+sf::Vector2f Player::BodyPart::GetBodyPartScreenPosition()
+{
+	float x = LevelView::border_offset_left + (bodypart_grid_position.x * bodypart_width) + (bodypart_width / 2);
+	float y = LevelView::border_offset_top + (bodypart_grid_position.y * bodypart_height) + (bodypart_height / 2);
+	return sf::Vector2f(x,y);
+}
+
+void BodyPart::Destroy()
+{
+	delete(bodypart_image);
+}
+
+BodyPart::BodyPart()
+{
+	bodypart_image = nullptr;
+	bodypart_grid_position = sf::Vector2i(0, 0);
+	CreateBodyPartImage();
+}
+
+BodyPart::~BodyPart()
+{
+	Destroy();
+}
+
+void BodyPart::Initialize(float width, float height, sf::Vector2i position, Direction direction)
+{
+	bodypart_width = width;
+	bodypart_height = height;
+	bodypart_grid_position = position;
+	bodypart_direction = direction;
+
+	InitializeBodyPartImage();
+}
+
+void BodyPart::Render()
+{
+	bodypart_image->render();
+}
