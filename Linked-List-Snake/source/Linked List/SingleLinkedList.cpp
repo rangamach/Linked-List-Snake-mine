df252cb80aaba5cce2785360ebc9f1d085f0cb24
rangamach <117797904+rangamach@users.noreply.github.com>
2025-01-28 13:06:57 +0530

    insert node at tail

--- Linked-List-Snake/source/Linked List/SingleLinkedList.cpp
+++ Linked-List-Snake/source/Linked List/SingleLinkedList.cpp
@@ -18,26 +18,6 @@ sf::Vector2i SingleLinkedList::GetNewNodePosition(Node* ref_node, Operation oper
 		return ref_node->body_part.GetPrevPosition();
 	}
 	return default_position;
-
-	//Direction ref_direction = ref_node->body_part.GetDirection();
-	//sf::Vector2i ref_position = ref_node->body_part.GetPosition();
-
-	//switch (ref_direction)
-	//{
-	//case Direction::Up:
-	//	return sf::Vector2i(ref_position.x, ref_position.y - 1);
-	//	break;
-	//case Direction::Down:
-	//	return sf::Vector2i(ref_position.x, ref_position.y + 1);
-	//	break;
-	//case Direction::Left:
-	//	return sf::Vector2i(ref_position.x + 1, ref_position.y);
-	//	break;
-	//case Direction::Right:
-	//	return sf::Vector2i(ref_position.x - 1, ref_position.y);
-	//	break;
-	//}
-	//return default_position;
 }
 
 void SingleLinkedList::InitializeNode(Node* new_node, Node* ref_node, Operation operation)
@@ -79,19 +59,19 @@ void SingleLinkedList::Render()
 
 void SingleLinkedList::InsertNodeAtTail()
 {
+	linked_list_size++;
 	Node* new_node = CreateNode();
-	Node* current_node = head_node;
-
-	if (current_node == nullptr)
+	Node* cur_node = head_node;
+	if (cur_node == nullptr)
 	{
 		head_node = new_node;
-		new_node->body_part.Initialize(node_width, node_height, default_position, default_direction);
+		InitializeNode(new_node, nullptr, Operation::Tail);
 		return;
 	}
-	while (current_node->next != nullptr)
-		current_node = current_node->next;
-	current_node->next = new_node;
-	new_node->body_part.Initialize(node_width, node_height, GetNewNodePosition(current_node), current_node->body_part.GetDirection());
+	while (cur_node == nullptr)
+		cur_node = cur_node->next;
+	cur_node->next = new_node;
+	InitializeNode(new_node, cur_node, Operation::Tail);
 }
 
 void SingleLinkedList::InsertNodeAtHead()
