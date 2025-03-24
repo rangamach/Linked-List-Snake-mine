8598acc2afc2354dd301ae9e7b7eafaaf735a8aa
rangamach <117797904+rangamach@users.noreply.github.com>
2025-03-24 16:15:27 +0530

    remove half nodes

--- Linked-List-Snake/source/Linked List/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/Linked List/DoubleLinked/DoubleLinkedList.cpp
@@ -165,6 +165,19 @@ namespace LinkedList
 		}
 		void DoubleLinkedList::RemoveHalfNodes()
 		{
+			if (linked_list_size <= 1) return;
+			int half_length = linked_list_size / 2;
+			int new_tail_index = half_length - 1;
+			Node* prev_node = FindNodeAtIndex(new_tail_index);
+			Node* cur_node = prev_node->next;
+			while (cur_node != nullptr)
+			{
+				Node* node_to_delete = cur_node;
+				cur_node = cur_node->next;
+				delete(node_to_delete);
+				linked_list_size--;
+			}
+			prev_node->next = nullptr;
 		}
 		void DoubleLinkedList::ShiftNodeAfterRemoval(Node* cur_node)
 		{
